270b0ab85fb16e7ad7fc71cf36bf9f1f2b50a4d1
unknown <manuahnas@gmail.com>
2021-12-08 17:02:32 +0530

    first commit

---
+++ web/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+app_name = 'web'
+
+urlpatterns = [ 
+    path('', views.index,name="index"), 
+    path('about', views.about,name="about"), 
+    path('service', views.service,name="service"),
+    path('product', views.product,name="product"), 
+    path('productsingle', views.productsingle,name="productsingle"),
+    path('project', views.project,name="project"),
+    path('uproject', views.uproject,name="uproject"), 
+    path('blog', views.blog,name="blog"), 
+    path('blogsingle', views.blogsingle,name="blogsingle"),
+    path('contact', views.contact,name="contact"),
+]
---
+++ web/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+import json 
+
+
+def index(request):
+    context = {
+        "is_index" : True 
+    }
+    return render(request, 'index.html',context)
+
+def about(request): 
+    context = {
+        "is_about" : True
+    }
+    return render(request, 'about.html',context)
+
+def service(request):
+    context = {
+        "is_service" : True 
+    }
+    return render(request, 'service.html',context)
+
+def product(request):
+    context = {
+        "is_product" : True 
+    }
+    return render(request, 'product.html',context)
+
+def productsingle(request):
+    context = {
+        "is_productsingle" : True
+    }
+    return render(request, 'productsingle.html',context)
+
+def project(request):
+    context = {
+        "is_project" : True
+    }
+    return render(request, 'project.html',context) 
+
+def uproject(request):
+    context = {
+        "is_uproject" : True
+    }
+    return render(request, 'uproject.html',context) 
+
+def blog(request):
+    context = {
+        "is_blog" : True
+    }
+    return render(request, 'blog.html',context) 
+
+def blogsingle(request):
+    context = {
+        "is_blogsingle" : True
+    }
+    return render(request, 'blogsingle.html',context)
+
+def contact(request):
+    context = {
+        "is_contact" : True
+    }
+    return render(request, 'contact.html',context)
