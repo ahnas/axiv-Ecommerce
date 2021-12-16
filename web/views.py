3f288a62a35d8ba3623fdd73d483dc809d32fb85
unknown <manuahnas@gmail.com>
2021-12-16 14:35:33 +0530

    invoice balanc

--- web/views.py
+++ web/views.py
@@ -1,49 +1,52 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.http.response import JsonResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 import json
 
-from core.models import Cart,Order,CheckOuted
-from .models import CompletedProject, Partner, ProductCategory,Product,Blog, Project, ProjectCategory,Testimonial,Slider,Director
-from .forms import ContactForm,ServiceEnquiryForm,OrderForm
+from core.models import Cart, Order, CheckOuted
+from .models import CompletedProject, Partner, ProductCategory, Product, Blog, Project, ProjectCategory, Testimonial, Slider, Director
+from .forms import ContactForm, ServiceEnquiryForm, OrderForm
+
 
 def index(request):
     if request.session.session_key == None:
-        request.session.create() 
-        request.session['axiva']='initilise'
-    
+        request.session.create()
+        request.session['axiva'] = 'initilise'
+
     projectcategory = ProjectCategory.objects.all()
-    project =CompletedProject.objects.all()
+    project = CompletedProject.objects.all()
     slider = Slider.objects.all()
     blog = Blog.objects.filter().order_by('-id')[:3]
-    product = Product.objects.filter(is_dashboard = True)
+    product = Product.objects.filter(is_dashboard=True)
     partner = Partner.objects.all()
     testimonial = Testimonial.objects.all()
     context = {
-        "is_index" : True,
-        "product":product, 
-        "partner":partner,
-        "testimonial":testimonial,
-        "slider":slider,
-        "blog":blog,
-        "projectcategory":projectcategory,
-        "project":project,
+        "is_index": True,
+        "product": product,
+        "partner": partner,
+        "testimonial": testimonial,
+        "slider": slider,
+        "blog": blog,
+        "projectcategory": projectcategory,
+        "project": project,
     }
-    return render(request, 'index.html',context)
+    return render(request, 'index.html', context)
 
-def about(request): 
+
+def about(request):
     if request.session.session_key == None:
-        request.session.create() 
-        request.session['axiva']='initilise'
+        request.session.create()
+        request.session['axiva'] = 'initilise'
 
     director = Director.objects.all()
     context = {
-        "is_about" : True,
-        "director":director,
+        "is_about": True,
+        "director": director,
     }
-    return render(request, 'about.html',context)
+    return render(request, 'about.html', context)
+
 
 def service(request):
     form = ServiceEnquiryForm(request.POST or None)
@@ -51,89 +54,97 @@ def service(request):
         if form.is_valid():
             form.save()
             response_data = {
-                "status" : "true",
-                "title" : "Successfully Submitted",
-                "message" : "Contact You Soon"
+                "status": "true",
+                "title": "Successfully Submitted",
+                "message": "Contact You Soon"
             }
         else:
-            print (form.errors)
+            print(form.errors)
             response_data = {
-                "status" : "false",
-                "title" : "Form validation error",
+                "status": "false",
+                "title": "Form validation error",
             }
         return HttpResponse(json.dumps(response_data), content_type='application/javascript')
     else:
         context = {
-            "is_service" : True ,
-            "form":form,
+            "is_service": True,
+            "form": form,
         }
-    return render(request, 'service.html',context)
+    return render(request, 'service.html', context)
+
 
 def product(request):
     if request.session.session_key == None:
-        request.session.create() 
-        request.session['axiva']='initilise'
+        request.session.create()
+        request.session['axiva'] = 'initilise'
 
     productcategory = ProductCategory.objects.all()
     product = Product.objects.all()
     context = {
-        "is_product" : True ,
-        "productcategory":productcategory,
-        "product":product,
+        "is_product": True,
+        "productcategory": productcategory,
+        "product": product,
     }
-    return render(request, 'product.html',context)
+    return render(request, 'product.html', context)
 
-def productsingle(request,slug):
+
+def productsingle(request, slug):
     if request.session.session_key == None:
-        request.session.create() 
-        request.session['axiva']='initilise'
-        
-    category =  Product.objects.get(slug = slug)
+        request.session.create()
+        request.session['axiva'] = 'initilise'
+
+    category = Product.objects.get(slug=slug)
     getCategory = category.productcategory.id
-    similiarproduct = Product.objects.filter(productcategory_id = getCategory).exclude(slug = slug)
+    similiarproduct = Product.objects.filter(
+        productcategory_id=getCategory).exclude(slug=slug)
     product = get_object_or_404(Product, slug=slug)
     context = {
-        "is_productsingle" : True,
-        "product":product,
-        "similiarproduct":similiarproduct,
+        "is_productsingle": True,
+        "product": product,
+        "similiarproduct": similiarproduct,
     }
     for i in similiarproduct:
         print(i)
-    return render(request, 'productsingle.html',context)
+    return render(request, 'productsingle.html', context)
+
 
 def project(request):
     context = {
-        "is_project" : True
+        "is_project": True
     }
-    return render(request, 'project.html',context) 
+    return render(request, 'project.html', context)
+
 
 def uproject(request):
     project = Project.objects.all()
     context = {
-        "is_uproject" : True,
-        "project":project,
+        "is_uproject": True,
+        "project": project,
     }
-    return render(request, 'uproject.html',context) 
+    return render(request, 'uproject.html', context)
+
 
 def blog(request):
     blog = Blog.objects.filter().order_by('-date')
     context = {
-        "is_blog" : True,
-        "blog":blog,
+        "is_blog": True,
+        "blog": blog,
     }
-    return render(request, 'blog.html',context) 
+    return render(request, 'blog.html', context)
 
-def blogsingle(request,slug):
-    similiarblog = Blog.objects.all().exclude(slug = slug)
+
+def blogsingle(request, slug):
+    similiarblog = Blog.objects.all().exclude(slug=slug)
     blog = get_object_or_404(Blog, slug=slug)
     context = {
-        "is_blogsingle" : True,
-        "blog":blog,
-        "similiarblog":similiarblog,
+        "is_blogsingle": True,
+        "blog": blog,
+        "similiarblog": similiarblog,
     }
     for i in similiarblog:
         print(i)
-    return render(request, 'blogsingle.html',context)
+    return render(request, 'blogsingle.html', context)
+
 
 def contact(request):
     form = ContactForm(request.POST or None)
@@ -141,49 +152,52 @@ def contact(request):
         if form.is_valid():
             form.save()
             response_data = {
-                "status" : "true",
-                "title" : "Successfully Submitted",
-                "message" : "Message successfully updated"
+                "status": "true",
+                "title": "Successfully Submitted",
+                "message": "Message successfully updated"
             }
         else:
-            print (form.errors)
+            print(form.errors)
             response_data = {
-                "status" : "false",
-                "title" : "Form validation error",
+                "status": "false",
+                "title": "Form validation error",
             }
         return HttpResponse(json.dumps(response_data), content_type='application/javascript')
     else:
         context = {
-            "is_contact" : True,
-            "form":form,
+            "is_contact": True,
+            "form": form,
         }
-    return render(request, 'contact.html',context)
+    return render(request, 'contact.html', context)
 
 
-
-def cart(request): 
+def cart(request):
     sessionID = request.session.session_key
-    cartItem = Cart.objects.filter(session_key = sessionID)
+    cartItem = Cart.objects.filter(session_key=sessionID)
+
     class cart:
-        def __init__(self,product,quantity,price):
-            self.product=product
-            self.quantity=quantity
-            self.price=price
-    cartItems=[]
-    totalprice=0
-    if Cart.objects.filter(session_key = sessionID).exists():
+        def __init__(self, cnt, product, quantity, price):
+            self.cnt = cnt
+            self.product = product
+            self.quantity = quantity
+            self.price = price
+    cartItems = []
+    totalprice = 0
+    if Cart.objects.filter(session_key=sessionID).exists():
+        cnt = 1
+
         for item in cartItem:
-            totalprice+=int(item.quantity)*int(item.product.price)
-            cartItems.append(cart(item.product,item.quantity,int(item.quantity)*int(item.product.price)))
-            
+            totalprice += int(item.quantity)*int(item.product.price)
+            cartItems.append(cart(cnt, item.product, item.quantity,
+                             int(item.quantity)*int(item.product.price)))
+            cnt += 1
+
     context = {
-            "is_cart" : True,   
-            "cartItems":cartItems,
-            "totalprice":totalprice
-        }
-    return render(request, 'cart.html',context)
-
-
+        "is_cart": True,
+        "cartItems": cartItems,
+        "totalprice": totalprice
+    }
+    return render(request, 'cart.html', context)
 
 
 def checkout(request):
@@ -202,32 +216,67 @@ def checkout(request):
 
         orderID = orderSave.id
         date = orderSave.date
-        cartitems = Cart.objects.filter(session_key = request.session.session_key)
+        cartitems = Cart.objects.filter(
+            session_key=request.session.session_key)
+        cartitems1 = Cart.objects.filter(
+            session_key=request.session.session_key)
 
+        class cart:
+            def __init__(self, cnt, product, quantity, price):
+                self.product = product
+                self.cnt = cnt
+                self.quantity = quantity
+                self.price = price
+        cartItems = []
+        totalprice = 0
+        if Cart.objects.filter(session_key=request.session.session_key).exists():
+            cnt = 1
+            for item in cartitems1:
+                totalprice += int(item.quantity)*int(item.product.price)
+                cartItems.append(cart(cnt, item.product, item.quantity,
+                                      int(item.quantity)*int(item.product.price)))
+                cnt += 1
+        try:
+            print('Entr#'*12,len(cartItems))
+
+            # messagestring = 'https://wa.me/9660590099210?text=Name :'+name+'%0aAddress :'+address+'%0aPhone :'+phone +\
+            #     "%0a-----Order Details------"
+            
+            for cartItem in cartItems:
+                print(cartItem.product)
+
+            #     messagestring += "%0aProduct-Id:"+str(i['id'])+"%0aName:"+str(i['name'])+"%0aQty:"+str(
+            #         i['quantity'])+"%0aPrice:"+str(i['price'])+"%0aTotal :"+str(i['sub_total'])+"%0a-----------------------------"
+            # messagestring += "%0a-----------------------------%0a\
+            # Grand Total :"+str(grandtotal)+"%0a--------------------------------"
+            # del request.session['cartdata']
+
+        except:
+
+            pass
         userDetails = {
-            'name' :  name,
-            'address' :  address,
-            'number' :  number,
-            'orderID' :  orderID,
-            'date':date,
+            'name':  name,
+            'address':  address,
+            'number':  number,
+            'orderID':  orderID,
+            'date': date,
         }
-        
-        return render(request, 'checkout.html',{"cartitems":cartitems,"form":form,"invoice":True,"userDetails":userDetails})
-    return render(request, 'checkout.html',{"form":form,"invoice":False})
 
+        return render(request, 'checkout.html', {"totalprice": totalprice, "cartitems": cartItems, "form": form, "invoice": True, "userDetails": userDetails})
+    return render(request, 'checkout.html', {"form": form, "invoice": False})
 
 
 def confirmcheckout(request):
-    
-    cartitems = Cart.objects.filter(session_key = request.session.session_key)
+
+    cartitems = Cart.objects.filter(session_key=request.session.session_key)
     for items in cartitems:
-        checkout=CheckOuted()
-        checkout.date=items.date
-        checkout.session_key=items.session_key
-        checkout.product=items.product
-        checkout.quantity=items.quantity
-        checkout.status='open'
+        checkout = CheckOuted()
+        checkout.date = items.date
+        checkout.session_key = items.session_key
+        checkout.product = items.product
+        checkout.quantity = items.quantity
+        checkout.status = 'open'
         checkout.save()
-    
-    Cart.objects.filter(session_key = request.session.session_key).delete()
-    return JsonResponse({"success":"yes"})
+
+    Cart.objects.filter(session_key=request.session.session_key).delete()
+    return JsonResponse({"success": "yes"})
