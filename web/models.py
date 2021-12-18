1f59ddb803f77dafc77d28181ead3d28de37b9da
unknown <manuahnas@gmail.com>
2021-12-18 16:18:18 +0530

    STrac

--- web/models.py
+++ web/models.py
@@ -13,7 +13,7 @@ class Slider(models.Model):
     firstline = models.CharField(max_length=120,blank=True,null=True)
     secondline = models.CharField(max_length=120,blank=True,null=True)
     thirdline = models.CharField(max_length=120,blank=True,null=True)
-    photo = VersatileImageField(upload_to="Slider/")
+    photo = VersatileImageField(upload_to="Slider/",blank=True,null=True)
 
     def __str__(self):
         return str(self.firstline)
